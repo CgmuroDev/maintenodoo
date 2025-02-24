428fda6dc2d71d4b2bc6785ad3149a07c55e2944
carlos <gonzalezmurocarlosmanuel@gmail.com>
2025-02-24 17:17:25 -0600

    Agregar wizard para reporte de mantenimiento por fecha y mejorar la estructura del módulo

--- __manifest__.py
+++ __manifest__.py
@@ -1,30 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Mantenimiento",
-
+    'name': "Mantenimodoo",
     'summary': "Short (1 phrase/line) summary of the module's purpose",
-
     'description': """
 Long description of module's purpose
     """,
-
     'author': "My Company",
     'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'mail'],
-
-    # always loaded
     'data': [
         'data/ir_cron.xml',
         'reports/paperformat_custom.xml',
         'reports/mantenimiento_invoice_report_view.xml',
+        'reports/dates_external_layout.xml',  # Asegúrate de incluirlo
         'security/mantenimiento_segurity.xml',
         'security/ir.model.access.csv',
         'views/equipo_sequence.xml',
@@ -36,7 +26,6 @@ Long description of module's purpose
         'views/templates.xml',
         'wizard/date_report.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
--- wizard/date_wizard.py
+++ wizard/date_wizard.py
@@ -1,18 +1,46 @@
-from odoo import fields, models, api
-import logging
+from odoo import models, fields, api
 
-_logger = logging.getLogger(__name__)
 
 class DateReportWizard(models.TransientModel):
     _name = 'maintenodoo.date.report.wizard'
-    _description = 'Date Report Wizard'
+    _description = 'Wizard para reporte de mantenimiento'
 
-    start_date = fields.Date(string='Fecha Inicial', required=True)
-    end_date = fields.Date(string='Fecha Final', required=True)
+    start_date = fields.Date(string='Fecha de inicio', required=True)
+    end_date = fields.Date(string='Fecha de fin', required=True)
     mantenimientos_ids = fields.Many2many('maintenodoo.mantenimiento', string='Mantenimientos')
 
+    @api.onchange('start_date', 'end_date')
+    def onchange_dates(self):
+        print("Ejecutando onchange_dates")
+        if self.start_date and self.end_date:
+            mantenimientos = self.env['maintenodoo.mantenimiento'].search([
+                ('programed_date', '>=', self.start_date),
+                ('executed_date', '<=', self.end_date)
+            ])
+            print(f"Mantenimientos encontrados: {len(mantenimientos)}")
+            self.mantenimientos_ids = [(6, 0, mantenimientos.ids)]
 
     def print_report(self):
 
-        _logger.info('Imprimiendo Reporte de Mantenimientos')
-        return True
+        manteniemientos = self.env['maintenodoo.mantenimiento']
+
+        domain = [
+            ('programed_date', '>=', self.start_date),
+            ('executed_date', '<=', self.end_date)
+        ]
+        if self.mantenimientos_ids:
+            domain.append(('id', 'in', self.mantenimientos_ids.ids))
+
+        manfields = ['name', 'programed_date', 'executed_date', 'equipment_id']
+
+        manrecords = manteniemientos.search_read(domain, manfields)
+
+        data = {
+            'manrecords': manrecords,
+            'start_date': self.start_date,
+            'end_date': self.end_date
+        }
+
+        return self.env.ref('maintenodoo.report_mantenimiento_fecha').report_action(self, data=data)
+
+
